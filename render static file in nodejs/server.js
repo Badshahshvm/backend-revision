a933687f8b1b59b0920b9534b2e29d1c38ee9746
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-02-03 19:55:21 +0530

    Update server.js

--- render static file in nodejs/server.js
+++ render static file in nodejs/server.js
@@ -1,1 +1,11 @@
 const express=require("express");
+const path=require("path");
+const PORT=4000
+const app=express();
+const publicPath=path.join(__dirname,"public");
+
+app.use(express.static(publicPath))
+app.listen(PORT,()=>
+{
+              console.log("server is running");
+})
